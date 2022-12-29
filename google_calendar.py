080995222bf965f3c89cc92dcffa9f28f363a4f4
Zi Yan Wu <ziyanwu@mit.edu>
2022-12-28 23:51:10 -0800

    standardized timezone things and added manual credentials refresh

--- google_calendar.py
+++ google_calendar.py
@@ -1,10 +1,16 @@
 import os
 import datetime
+import api_keys
 
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
+
+## TODO: bind timezone to location (possibly get location from the weather) instead of hardcoding it
+## 
+TIMEZONE = datetime.timezone(datetime.timedelta(hours=-5))
+
 
 """ When using a service account """
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "google_service_account.json"
@@ -17,17 +23,19 @@ from google.auth.transport.requests import Request
 """ Trying to use Oauth without having to open the browser every single time """
 # Good link: https://stackoverflow.com/questions/73788841/how-to-auto-refresh-google-calendar-token-in-python 
 
-# The library might actually just refresh it automatically for us.  
+  
 credentials = Credentials.from_authorized_user_file('calendar_creds.json')
 if not(credentials.valid):
     # If the credentials stored are no longer valid, refresh it
     print('credentials refreshed!')
-    credentials.refresh(Request())
     
-    ## TODO:
-    ## Currently, the refresh token does its job and gets us valid credentials
-    ## But eventually, the refresh token itself will expire.
-    ## Create a fallback measure where we do the manual login authentication again to get fresh credentials
+    try:
+        credentials.refresh(Request())
+    except:
+        # For RefreshError, when the refresh token itself expires. We have to manually reauthenticate.
+        flow = InstalledAppFlow.from_client_secrets_file('google_oauth_credentials.json',scopes=["https://www.googleapis.com/auth/calendar.readonly"])
+        flow.run_local_server()
+        credentials = flow.credentials
 
     # Save the credentials for the next run
     with open('calendar_creds.json','wt') as f:
@@ -45,7 +53,7 @@ def get_events(start_date):
 
 
     Inputs:
-        start_date: A datetime.datetime object
+        start_date: A datetime.datetime object (timezone info MUST be included with this object)
     
     Returns:
         Returns a list of events on the calendar. Each event is a dictionary containing information about that event.
@@ -53,7 +61,7 @@ def get_events(start_date):
     """
     end_date = start_date + datetime.timedelta(days=1)
 
-    events = service.events().list(calendarId='ziyanwu3@gmail.com',timeMin=start_date.isoformat(),timeMax=end_date.isoformat()).execute()
+    events = service.events().list(calendarId = api_keys.my_email ,timeMin=start_date.isoformat(),timeMax=end_date.isoformat()).execute()
     return events['items']
 
 
@@ -67,16 +75,16 @@ def get_times(events,start_date):
         start_date: A datetime.datetime object
     
     Output:
-        Returns a set of the start times for each event. Each start time is a datetime.time object
+        Returns a set of the start times for each event. Each start time is a datetime.datetime object
 
     """
     out = set()
 
     for event in events:
         event_start_datetime = datetime.datetime.fromisoformat(event['start']['dateTime'])
-        if event_start_datetime.day == start_date.day:
-            time = event_start_datetime.timetz()
-            out.add(time)
+        consistent_date = event_start_datetime.astimezone(tz=TIMEZONE)
+        if consistent_date.day == start_date.day:
+            out.add(consistent_date)
 
     return out
 
@@ -89,8 +97,7 @@ if __name__ == '__main__':
     # date = datetime.datetime.now(datetime.timezone.utc).isoformat())
 
     # December 18, 2022 at 12am EST time
-    date = datetime.datetime(2022,12,18,tzinfo=datetime.timezone(-datetime.timedelta(hours=5)))
-    
+    date = datetime.datetime(2022,12,18,tzinfo=TIMEZONE)
     events = get_events(date)
     print(get_times(events,date))
     
